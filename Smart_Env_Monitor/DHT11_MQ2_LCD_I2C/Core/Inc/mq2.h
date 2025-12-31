1492d9361754ce39297af05a3c86e506f252fc84
Manasi0104 <23uet119@dkte.ac.in>
2025-12-31 15:33:01 +0530

    ARM Final Project

---
+++ Smart_Env_Monitor/DHT11_MQ2_LCD_I2C/Core/Inc/mq2.h
@@ -0,0 +1,22 @@
+/*
+ * mq2.h
+ *
+ *  Created on: Dec 30, 2025
+ *      Author: ADMIN
+ */
+
+#ifndef INC_MQ2_H_
+#define INC_MQ2_H_
+#include "main.h"
+/* Standard C libraries */
+#include <stdio.h>      // for
+#include <string.h>     // for string handling
+#include <stdarg.h>     // for variable arguments
+
+/* STM32 HAL library */
+#include "stm32f4xx_hal.h"
+void MX_ADC1_Init(void);
+uint16_t read_adc(void);
+void mq2_operation(uint16_t val);
+extern ADC_HandleTypeDef hadc1;
+#endif /* INC_MQ2_H_ */
